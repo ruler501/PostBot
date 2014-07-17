14d3b80c719881b96715f3af1172f5048575f651
Devon Richards <d.ruler501@gmail.com>
2014-07-16 22:40:37 -0500

    Fix various problems

--- PostBot.py
+++ PostBot.py
@@ -15,6 +15,7 @@ network = 'irc.freenode.net'
 port = 6667
 chan='#acreloaded-forum'
 last = time.time()
+connected = False
 rssurl = "http://forum.acr.victorz.ca/syndication2.php?limit=5"
 posts = []
 
@@ -27,31 +28,42 @@ print(irc.recv(4096))
 irc.recv(4096)
 irc.send(bytes('NICK ' + nick + '\r\n', 'UTF-8'))
 irc.send(bytes('USER LEAP LEAPer LEAPer :LEAP IRC\r\n', 'UTF-8'))
+irc.settimeout(1)
 while True:
-	data = str(irc.recv(4096))
-	if debug:
+	try:
+		data = str(irc.recv(4096))
+	except socket.timeout:
+		data = ""
+	if debug and data != "":
 		print(data)
 	if data.find('PING') != -1: 
 		irc.send(bytes('PONG ' + data.split()[1] + '\r\n', 'UTF-8'))
 	if data.find('End of /MOTD command.') != -1: #check for welcome message
 		irc.send(bytes('JOIN ' + chan + '\r\n', 'UTF-8'))
+	if data.find(chan) != -1: #check for welcome message
+		connected = True
+		if debug:
+			print("connected")
 	if data.find('!quit')!=-1 and data.find('ruler501')!=-1:
 		break
-	times = millis = time.time()
-	if times-last > 8:
-		last = times
+	times = time.time()
+	if int(times-last)%2 == 0 and connected:
 		feed=feedparser.parse(rssurl)
 		for ent in feed.entries:
 			title = ent.title
 			if title[:4] == "RE: ":
-				title = ent.title[4:]
-			if (title+','+str(time.mktime(ent.published_parsed))) not in posts:
-				posts.append(title+','+str(time.mktime(ent.published_parsed))) #I think this is unique in practice, but theoretically could get duplicated
+				title = title[4:]
+			if ent.id not in posts:
+				print(posts)
+				posts.append(ent.id) #I think this is unique in practice, but theoretically could get duplicated
 				if len(title) > 30:
 					title = title[:26]+'...'#limits it to 30 characters(len(title[:26])==27)
 				out = title+' '+ent.id
 				if debug:
 					print(out)
 				sendMessage(out)
-				while len(posts) > 5:
+				while len(posts) > 10:
 					posts.pop(0)
+			elif debug:
+				print(ent.title+' '+ent.id)
+				print(posts)
